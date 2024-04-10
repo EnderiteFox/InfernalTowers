f7fe67f10f94028fc693282127b4d73d27e9b9b0
EndersteveGamer <85014963+EndersteveGamer@users.noreply.github.com>
2024-04-10 10:12:07 +0200

    Added implementation for Position

---
+++ src/core/ImplPosition.java
@@ -0,0 +1,49 @@
+package core;
+
+import api.Position;
+
+public class ImplPosition implements Position {
+    private int x;
+    private int y;
+    private int z;
+
+    public ImplPosition(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    @Override
+    public int getX() {
+        return x;
+    }
+
+    @Override
+    public int getY() {
+        return y;
+    }
+
+    @Override
+    public int getZ() {
+        return z;
+    }
+
+    @Override
+    public Position add(int x, int y, int z) {
+        this.x += x;
+        this.y += y;
+        this.z += z;
+        return this;
+    }
+
+    @Override
+    public Position add(Position other) {
+        add(other.getX(), other.getY(), other.getZ());
+        return this;
+    }
+
+    @Override
+    public Position clone() {
+        return new ImplPosition(x, y, z);
+    }
+}
